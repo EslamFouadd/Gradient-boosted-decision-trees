db6e7b10bfe8edffc09ebecab02b89182b9c9739
Sam Fouad <77150715+EslamFouadd@users.noreply.github.com>
2023-07-30 08:05:11 +0300

    Delete GBDTRegresor.py

--- GBDTRegresor.py
+++
@@ -1,7 +0,0 @@
-class GBDTRegressor(GBDTEstimator):
-
-    def calc_grad(self, y_true: np.ndarray, y_pred: np.ndarray) \
-            -> Tuple[np.ndarray, np.ndarray]:
-        grad = y_pred - y_true
-        hess = np.ones(len(y_true))
-        return grad, hess
